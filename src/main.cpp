9dabe41d6b46ef5dd3b7cb440ac05f4f1f3b021d
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2025-03-28 15:03:47 +0100

    Switching tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,102 @@
-import dx12.app;
-#include "libraries.h"
+#include "D3D12Lite.h"
+using namespace D3D12Lite;
 
-int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
-{
-    // Windows 10 Creators update adds Per Monitor V2 DPI awareness context.
-    // Using this awareness context allows the client area of the window
-    // to achieve 100% scaling while still allowing non-client window content to
-    // be rendered in a DPI sensitive fashion.
-    SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+import dx12.rdr;
+using namespace dx12;
 
-    //dx12::Application{hInstance};
+LRESULT CALLBACK WndProc(HWND hwnd, UINT umessage, WPARAM wparam, LPARAM lparam) {
+    switch (umessage) {
+    case WM_KEYDOWN:
+        if (wparam == VK_ESCAPE) {
+            PostQuitMessage(0);
+            return 0;
+        } else {
+            return DefWindowProc(hwnd, umessage, wparam, lparam);
+        }
+
+    case WM_DESTROY:
+        [[fallthrough]];
+    case WM_CLOSE:
+        PostQuitMessage(0);
+        return 0;
+
+    default:
+        return DefWindowProc(hwnd, umessage, wparam, lparam);
+    }
+}
+
+
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow) {
+    std::string applicationName = "D3D12 Tutorial";
+    Uint2 windowSize = {1280, 720};
+    HINSTANCE moduleHandle = GetModuleHandle(nullptr);
+
+    WNDCLASSEX wc = {0};
+    wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = moduleHandle;
+    wc.hIcon = LoadIcon(nullptr, IDI_WINLOGO);
+    wc.hIconSm = wc.hIcon;
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = applicationName.c_str();
+    wc.cbSize = sizeof(WNDCLASSEX);
+    RegisterClassEx(&wc);
+
+    int screenWidth = ::GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = ::GetSystemMetrics(SM_CYSCREEN);
+
+    RECT windowRect = { 0, 0, static_cast<LONG>(windowSize.x), static_cast<LONG>(windowSize.y) };
+    ::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    int windowWidth = windowRect.right - windowRect.left;
+    int windowHeight = windowRect.bottom - windowRect.top;
+
+    // Center the window within the screen. Clamp to 0, 0 for the top-left corner.
+    int windowX = std::max<int>(0, (screenWidth - windowWidth) / 2);
+    int windowY = std::max<int>(0, (screenHeight - windowHeight) / 2);
+
+    HWND windowHandle = CreateWindowEx(
+                NULL,
+                applicationName.c_str(),
+                applicationName.c_str(),
+                WS_OVERLAPPEDWINDOW,
+                windowX,
+                windowY,
+                windowWidth,
+                windowHeight,
+                NULL,
+                NULL,
+                moduleHandle,
+                nullptr
+            );
+
+    ShowWindow(windowHandle, SW_SHOW);
+    SetForegroundWindow(windowHandle);
+    SetFocus(windowHandle);
+    ShowCursor(true);
+
+    std::unique_ptr<Renderer> renderer = std::make_unique<Renderer>(windowHandle, windowSize);
+
+    bool shouldExit = false;
+    while (!shouldExit) {
+        MSG msg{0};
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        if (msg.message == WM_QUIT) {
+            shouldExit = true;
+        }
+    }
+
+    DestroyWindow(windowHandle);
+    windowHandle = nullptr;
+
+    UnregisterClass(applicationName.c_str(), moduleHandle);
+    moduleHandle = nullptr;
+
     return 0;
 }
