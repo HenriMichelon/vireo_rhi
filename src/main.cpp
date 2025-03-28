275b69c5fadd3e1144809b17d0ab10b4daca60cb
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2025-03-28 11:01:00 +0100

    Displaying the window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 import app;
+#include "libraries.h"
 
-int main() {
-    dx12::Application{};
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
+{
+    // Windows 10 Creators update adds Per Monitor V2 DPI awareness context.
+    // Using this awareness context allows the client area of the window
+    // to achieve 100% scaling while still allowing non-client window content to
+    // be rendered in a DPI sensitive fashion.
+    SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
+    dx12::Application{hInstance};
     return 0;
 }
