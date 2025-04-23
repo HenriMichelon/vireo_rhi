c0bb84ef672bbbe4a245342de5062cd94640e083
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2025-04-23 13:31:02 +0200

    Add resource state transitions for SHADER_READ and UNDEFINED

--- src/directx/DXCommands.cpp
+++ src/directx/DXCommands.cpp
@@ -308,6 +308,12 @@ namespace vireo {
         } else if (oldState == ResourceState::RENDER_TARGET_COLOR && newState == ResourceState::COMPUTE_READ) {
             srcState = D3D12_RESOURCE_STATE_RENDER_TARGET;
             dstState = D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE;
+        } else if (oldState == ResourceState::RENDER_TARGET_COLOR && newState == ResourceState::SHADER_READ) {
+            srcState = D3D12_RESOURCE_STATE_RENDER_TARGET;
+            dstState = D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE | D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
+        } else if (oldState == ResourceState::SHADER_READ && newState == ResourceState::UNDEFINED) {
+            srcState = D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE | D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
+            dstState = D3D12_RESOURCE_STATE_COMMON;
         } else if (oldState == ResourceState::UNDEFINED && newState == ResourceState::RENDER_TARGET_DEPTH_STENCIL) {
             srcState = D3D12_RESOURCE_STATE_COMMON;
             dstState = D3D12_RESOURCE_STATE_DEPTH_WRITE;
