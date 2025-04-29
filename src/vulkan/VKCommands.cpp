d3ab1f9e3ce8c38d67b0d732595f1cbe0720d379
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2025-04-29 13:12:22 +0200

    Update pipeline stages for depth-stencil state transitions

--- src/vulkan/VKCommands.cpp
+++ src/vulkan/VKCommands.cpp
@@ -583,14 +583,14 @@ namespace vireo {
             dstLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
         } else if (oldState == ResourceState::UNDEFINED && newState == ResourceState::RENDER_TARGET_DEPTH_STENCIL) {
             srcStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
-            dstStage = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+            dstStage = VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
             srcAccess = 0;
             dstAccess = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
             srcLayout = VK_IMAGE_LAYOUT_UNDEFINED;
             dstLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
             aspectFlag = VK_IMAGE_ASPECT_DEPTH_BIT;
         } else if (oldState == ResourceState::RENDER_TARGET_DEPTH_STENCIL && newState == ResourceState::RENDER_TARGET_DEPTH_STENCIL_READ) {
-            srcStage = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+            srcStage = VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
             dstStage = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
             srcAccess = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
             dstAccess = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT;
