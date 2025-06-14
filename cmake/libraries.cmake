eaaa1fe9528f841ee509a9024bb46a5ca3fdbede
Henri <148531229+HenriMichelon@users.noreply.github.com>
2025-06-14 15:54:52 +0200

    Improve DirectX backend configuration handling

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -10,12 +10,12 @@ find_package(Vulkan REQUIRED)
 
 ###### DirectX Headers
 if (DIRECTX_BACKEND)
-message(NOTICE "Fetching DirectX-Headers from https://github.com/microsoft/DirectX-Headers.git...")
-FetchContent_Declare(
-        DirectX-Headers
-        GIT_REPOSITORY https://github.com/microsoft/DirectX-Headers.git
-        GIT_TAG main
-)
-FetchContent_MakeAvailable(DirectX-Headers)
-set(DirectXH_DIR ${CMAKE_BINARY_DIR}/_deps/directx-headers-src)
+    message(NOTICE "Fetching DirectX-Headers from https://github.com/microsoft/DirectX-Headers.git...")
+    FetchContent_Declare(
+            DirectX-Headers
+            GIT_REPOSITORY https://github.com/microsoft/DirectX-Headers.git
+            GIT_TAG main
+    )
+    FetchContent_MakeAvailable(DirectX-Headers)
+    set(DirectXH_DIR ${CMAKE_BINARY_DIR}/_deps/directx-headers-src)
 endif ()
