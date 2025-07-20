ee9f711c725d52170f936dd6c594b8f1c339e01a
Henri <148531229+HenriMichelon@users.noreply.github.com>
2025-07-20 19:58:03 +0200

    Adding Font

--- src/directx/DXCommands.cpp
+++ src/directx/DXCommands.cpp
@@ -672,6 +672,9 @@ namespace vireo {
         if (oldState == ResourceState::INDIRECT_DRAW && newState == ResourceState::COPY_DST) {
             srcState = D3D12_RESOURCE_STATE_INDIRECT_ARGUMENT;
             dstState = D3D12_RESOURCE_STATE_COPY_DEST;
+        }else if (oldState == ResourceState::UNDEFINED && newState == ResourceState::COPY_DST) {
+            srcState = D3D12_RESOURCE_STATE_COMMON;
+            dstState = D3D12_RESOURCE_STATE_COPY_DEST;
         } else if (oldState == ResourceState::COPY_DST && newState == ResourceState::COMPUTE_WRITE) {
             srcState = D3D12_RESOURCE_STATE_COPY_DEST;
             dstState = D3D12_RESOURCE_STATE_UNORDERED_ACCESS;
@@ -718,6 +721,7 @@ namespace vireo {
             srcState = D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE | D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
             dstState = D3D12_RESOURCE_STATE_COPY_DEST;
         } else if (oldState == ResourceState::COPY_DST && newState == ResourceState::VERTEX_INPUT) {
+            srcState = D3D12_RESOURCE_STATE_COPY_DEST;
             dstState = D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER;
         } else if (oldState == ResourceState::COPY_SRC && newState == ResourceState::INDIRECT_DRAW) {
             srcState = D3D12_RESOURCE_STATE_COPY_SOURCE;
