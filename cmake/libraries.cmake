d7cbccd285b8b05341a15b88017f052062e849b6
Henri Michelon <hmichelon@mailcourrier.fr>
2026-05-04 14:41:08 +0200

    Update SDL 3

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -37,7 +37,7 @@ if (USE_SDL3)
     FetchContent_Declare(
             SDL3
             GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-            GIT_TAG        release-3.4.4
+            GIT_TAG        release-3.4.8
     )
     set(SDL_SHARED OFF CACHE BOOL "" FORCE)
     set(SDL_STATIC ON  CACHE BOOL "" FORCE)
